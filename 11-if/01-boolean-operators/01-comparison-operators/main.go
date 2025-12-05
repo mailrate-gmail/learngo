8635b619fc2399d81a547114f5813255225925a0
mailrate-gmail <mailrate@gmail.com>
2025-12-05 13:43:35 +0800

    Update main.go

--- 11-if/01-boolean-operators/01-comparison-operators/main.go
+++ 11-if/01-boolean-operators/01-comparison-operators/main.go
@@ -10,18 +10,34 @@ package main
 
 import "fmt"
 
+// func main() {
+// 	speed := 100 // #1
+// 	// speed := 10 // #2
+
+// 	fast := speed >= 80
+// 	slow := speed < 20
+
+// 	fmt.Printf("fast's type is %T\n", fast)
+
+// 	fmt.Printf("going fast? %t\n", fast)
+// 	fmt.Printf("going slow? %t\n", slow)
+
+//		fmt.Printf("is it 100 mph? %t\n", speed == 10)
+//		fmt.Printf("is it not 100 mph? %t\n", speed != 100)
+//	}
 func main() {
-	speed := 100 // #1
-	// speed := 10 // #2
+	var speed int
+	fmt.Print("请输入速度: ")
+	fmt.Scanln(&speed)
 
 	fast := speed >= 80
 	slow := speed < 20
 
-	fmt.Printf("fast's type is %T\n", fast)
-
-	fmt.Printf("going fast? %t\n", fast)
-	fmt.Printf("going slow? %t\n", slow)
-
-	fmt.Printf("is it 100 mph? %t\n", speed == 100)
-	fmt.Printf("is it not 100 mph? %t\n", speed != 100)
+	if fast {
+		fmt.Println("fast")
+	} else if slow {
+		fmt.Println("slow")
+	} else {
+		fmt.Println("normal")
+	}
 }
